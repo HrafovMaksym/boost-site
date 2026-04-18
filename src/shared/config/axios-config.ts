fabbfc86f0f0de0422c475e58d281ac23bb7300e
HrafovMaksym <mgrafov06@gmail.com>
2026-04-18 02:44:54 +0200

    fix

--- src/shared/config/axios-config.ts
+++ src/shared/config/axios-config.ts
@@ -1,7 +1,7 @@
 import axios, { type InternalAxiosRequestConfig } from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://carryme.cc/api",
   withCredentials: true,
   headers: { "Content-Type": "application/json" },
 });
