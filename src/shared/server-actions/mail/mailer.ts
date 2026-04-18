8bd10b04387c29bab04b1cff4b221173eb97f46d
HrafovMaksym <mgrafov06@gmail.com>
2026-04-18 02:39:31 +0200

    fix

--- src/shared/server-actions/mail/mailer.ts
+++ src/shared/server-actions/mail/mailer.ts
@@ -1,8 +1,6 @@
 "use server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 interface SendMailOptions {
   to: string;
   subject: string;
@@ -10,6 +8,7 @@ interface SendMailOptions {
 }
 
 export async function sendMail({ to, subject, html }: SendMailOptions) {
+  const resend = new Resend(process.env.RESEND_API_KEY);
   const response = await resend.emails.send({
     from: process.env.SUPPORT_EMAIL!,
     to,
